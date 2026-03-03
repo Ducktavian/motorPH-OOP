7937dcf5dbd8aaada327a1c643da40e1a6700bd8
Hairam <mariahalvarez.study@gmail.com>
2026-03-03 22:11:39 +0800

    change frame size to 1000x700

--- src/com/motorph/ui/AttendancePanel.java
+++ src/com/motorph/ui/AttendancePanel.java
@@ -26,6 +26,8 @@ public class AttendancePanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        setPreferredSize(new java.awt.Dimension(1000, 700));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
--- src/com/motorph/ui/MainFrame.java
+++ src/com/motorph/ui/MainFrame.java
@@ -29,6 +29,7 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1000, 700));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
